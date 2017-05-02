d689fb7fd67d93f7a06cc8661bd2d32f8ab10c84
sawojcicki <sawojcicki@gmail.com>
2017-05-02 19:59:58 -0400

    Update oildetails.js

--- app/assets/scripts/views/oildetails.js
+++ app/assets/scripts/views/oildetails.js
@@ -254,16 +254,16 @@ var OilDetails = BaseView.extend({
     // Default model data
     var defaultModelData = {
       info: Oci.data.info,
-      opgee: Oci.Collections.opgee.get(utils.getOPGEEModel('0', '0', '0')).toJSON(),
-      prelim: Oci.Collections.prelim.get(utils.getPRELIMModel('0 = Default', 1)).toJSON()
+      opgee: Oci.Collections.opgee.get(utils.getOPGEEModel('0','0', '0', '0')).toJSON(),
+      prelim: Oci.Collections.prelim.get(utils.getPRELIMModel('0', '0 = Default', 1)).toJSON()
     };
 
     // Grab things based on the model we're using
     var params = this.modelParametersView.getModelValues();
 
     // if we don't have the necessary data, load it
-    var opgeeRun = utils.getOPGEEModel(params.solarSteam, params.water, params.flaring);
-    var prelimRun = utils.getPRELIMModel(params.refinery, params.lpg);
+    var opgeeRun = utils.getOPGEEModel(params.gwp, params.solarSteam, params.water, params.flaring);
+    var prelimRun = utils.getPRELIMModel(params.gwp, params.refinery, params.lpg);
     if (!Oci.Collections.opgee.get(opgeeRun)) {
       var opgeeModel = new OpgeeModel({ id: opgeeRun });
       opgeeModel.fetch({ async: false, success: function (data) {
