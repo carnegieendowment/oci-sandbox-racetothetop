0fec201e956ba0d58bb6f2dc04c8c0f0f73f930e
sawojcicki <sawojcicki@gmail.com>
2017-05-04 15:59:50 -0400

    Update precalc.js

--- .build_scripts/precalc.js
+++ .build_scripts/precalc.js
@@ -31,12 +31,10 @@ gi.forEach(function (_, g) {
   });
 });
 
-gi.forEach(function (_, g) {
-  ri.forEach(function (_, r) {
-    li.forEach(function (_, l) {
-      var temp = JSON.parse(fs.readFileSync('app/assets/data/prelim/prelim_run' + g + r + l + '.json'));
-      Oci.data.prelim['run' + g + r + l] = temp;
-    });
+ri.forEach(function (_, r) {
+  li.forEach(function (_, l) {
+    var temp = JSON.parse(fs.readFileSync('app/assets/data/prelim/prelim_run' + r + l + '.json'));
+    Oci.data.prelim['run' + g + r + l] = temp;
   });
 });
 
