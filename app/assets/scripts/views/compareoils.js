cb1a3e48a706723301ba9b4d032d233804dd3e1a
sawojcicki <sawojcicki@gmail.com>
2017-05-04 17:52:56 -0400

    Update compareoils.js

--- app/assets/scripts/views/compareoils.js
+++ app/assets/scripts/views/compareoils.js
@@ -127,7 +127,7 @@ var CompareOils = BaseView.extend({
 
     // if we don't have the necessary data, load it
     var opgeeRun = utils.getOPGEEModel(params.gwp, params.solarSteam, params.water, params.flaring);
-    var prelimRun = utils.getPRELIMModel(params.gwp, params.refinery, params.lpg);
+    var prelimRun = utils.getPRELIMModel(params.refinery, params.lpg);
     if (!Oci.Collections.opgee.get(opgeeRun)) {
       var opgeeModel = new OpgeeModel({ id: opgeeRun });
       opgeeModel.fetch({ async: false, success: function (data) {
