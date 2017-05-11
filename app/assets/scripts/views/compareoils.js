b3f7ddec7315a21d7cfe6b8f934fb3e2c3c96535
sawojcicki <sawojcicki@gmail.com>
2017-05-11 13:23:09 -0400

    Update compareoils.js

--- app/assets/scripts/views/compareoils.js
+++ app/assets/scripts/views/compareoils.js
@@ -26,6 +26,7 @@ var CompareOils = BaseView.extend({
     'click #compare-oils-share': 'handleShare',
     'click #price-button': 'showPrices',
     'change #toggle-lpg': 'handleParametersChange',
+    'change #toggle-gwp': 'handleParametersChange',
     'change .config-dropdown': 'handleDropdown',
     'click .mp-summary': 'handleParametersToggle',
     'change #sort-select': 'handleSortSelect',
@@ -127,7 +128,7 @@ var CompareOils = BaseView.extend({
 
     // if we don't have the necessary data, load it
     var opgeeRun = utils.getOPGEEModel(params.gwp, params.solarSteam, params.water, params.flaring);
-    var prelimRun = utils.getPRELIMModel(params.refinery, params.lpg);
+    var prelimRun = utils.getPRELIMModel(params.gwp, params.refinery, params.lpg);
     if (!Oci.Collections.opgee.get(opgeeRun)) {
       var opgeeModel = new OpgeeModel({ id: opgeeRun });
       opgeeModel.fetch({ async: false, success: function (data) {
