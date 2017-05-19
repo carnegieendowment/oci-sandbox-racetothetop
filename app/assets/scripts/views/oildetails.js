d7e5c97fd1867c0da9c2649962894cd602296690
sawojcicki <sawojcicki@gmail.com>
2017-05-19 16:00:05 -0400

    Update oildetails.js

--- app/assets/scripts/views/oildetails.js
+++ app/assets/scripts/views/oildetails.js
@@ -256,7 +256,7 @@ var OilDetails = BaseView.extend({
     var defaultModelData = {
       info: Oci.data.info,
       opgee: Oci.Collections.opgee.get(utils.getOPGEEModel('0', '0', '0', '0')).toJSON(),
-      prelim: Oci.Collections.prelim.get(utils.getPRELIMModel('0', '0 = Default', 1)).toJSON()
+      prelim: Oci.Collections.prelim.get(utils.getPRELIMModel(0, '0 = Default', 1)).toJSON()
     };
 
     // Grab things based on the model we're using
